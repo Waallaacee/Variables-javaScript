2b35003e610be20b9eb2c413e5d1aad00905cc7e
Waallaacee <cddwlc2@gmail.com>
2022-09-08 11:31:38 -0300

    Exercicio variavel

---
+++ index.js
@@ -0,0 +1,12 @@
+let nome, sobrenome, peso, altura, data_de_internação, diabético
+//alert (nome)
+//console.log
+nome = prompt ("escreva o seu nome:");
+sobrenome = prompt ("escreva seu sobrenome");
+peso = prompt ("qual é o seu peso?");
+altura = prompt ("qual a sua altura?");
+data_de_internação = prompt ("qual a data de internação?");
+diabético = prompt ("é diabetico?");
+alert("seu nome é: " + nome + "\nseu sobrenome é: " + sobrenome +
+ "\nseu peso é: " + peso + "\nsua altura é: " + altura + 
+ "\na data de internação é: " + data_de_internação + "\nvocê é diabético?: " + diabético)
